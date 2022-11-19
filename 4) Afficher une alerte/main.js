2e15070455ad0362a702ec5511331d70502173a7
Marine B <115531064+Kryyys@users.noreply.github.com>
2022-11-19 17:02:57 +0100

    Add files via upload

---
+++ 4) Afficher une alerte/main.js
@@ -0,0 +1,10 @@
+function texte() {
+    if (document.getElementById("message").value == "") {
+        alert("Marque un truc wesh");
+    } else {
+        alert (document.getElementById("message").value);
+    }
+    
+
+    
+}
